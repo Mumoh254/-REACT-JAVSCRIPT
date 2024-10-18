2c9b84d2ad7e33180855f189cbfa8ee6c7ba69a9
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2024-10-18 18:22:04 +0000

    u

---
+++ Props/usestate/src/rev.jsx
@@ -0,0 +1,30 @@
+
+
+import React from "react";
+import { useState } from "react";
+
+
+function Rev  (){
+
+
+    let countries  = [
+
+        {name:"kenya" , populatio:"29983848"},
+        {name:"kenya" , populatio:"29983848"}
+    ]
+
+    let [country  useCountry]  = useState(countries )
+
+    return(
+
+        <div>    
+
+           <h1> {country[0].name}</h1>
+           <h1> {country[0].population}</h1>
+        
+        </div>
+
+    )
+}
+
+expoprt default = Rev
