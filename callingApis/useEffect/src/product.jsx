5e5f53db726232624cc82193c88dfbe50b622a7a
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2024-10-19 02:40:01 +0000

    u

--- callingApis/useEffect/src/product.jsx
+++ callingApis/useEffect/src/product.jsx
@@ -1,7 +1,15 @@
 
-import React from "react";
+import React, { useEffect } from "react";
+
 
 function Product(){
+
+    useEffect( function(){
+
+        console.log("product  was   mounted ")
+
+    })
+
     return(
         <div  className="product">
 
