92d60ca793676547dfa22ed3e80f7d4889cce2d9
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2024-10-19 03:42:01 +0000

    u

--- callingApis/useEffect/src/App.jsx
+++ callingApis/useEffect/src/App.jsx
@@ -1,16 +1,33 @@
 
 
+import { useState } from 'react'
 import './App.css'
 import Product from './product'
 
 
 function App() {
   
+  let [showProduct , setShowProduct]  =  useState(false)
 
   return (
     <>
      <h1>useEffect  demo </h1>
-     <Product/>
+     <button  onClick={()=>{
+      setShowProduct(true)
+     }}> show </button>
+
+     <br />
+     <button  onClick={()=>{
+      setShowProduct(false)
+     }}> hide </button>
+
+
+     {
+      showProduct === true? (  <Product/>):(<h1>
+        component  is   still  mounted 
+      </h1>)
+     }
+   
     </>
   )
 }
