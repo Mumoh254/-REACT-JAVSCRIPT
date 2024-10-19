4bea036e5fb6bb6ae554fda7b9167422a7591014
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2024-10-19 02:59:59 +0000

    u

--- callingApis/useEffect/src/product.jsx
+++ callingApis/useEffect/src/product.jsx
@@ -1,20 +1,26 @@
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 
 function Product(){
+
+   let[product , setProduct]  =  useState("iphone  14  pro")
 
     useEffect( function(){
 
         console.log("product  was   mounted ")
 
-    })
+    },[product])
 
+    
     return(
         <div  className="product">
 
-      <h1>  Samsung</h1>
+      <h1>  {product}</h1>
       <h2> Price: 30000</h2>
+      <button  onClick={()=>{
+        setProduct("iphone  16  pro  max ")
+      }}>click </button>
 
         </div>
     )
